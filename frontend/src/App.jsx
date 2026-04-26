542c6ca23339b7199e1b42cf8f3c3fa3ba7acdb9
jagadeesh944078 <vemulajagadeesh28@gmail.com>
2026-04-26 18:50:43 +0530

    added ci cd workflow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
       <h1>Docker App 🚀</h1>
-      <button onClick={callApi}>Call Backend</button>
+      <button onClick={callApi}>Call Backend using React</button>
     </div>
   );
 };
