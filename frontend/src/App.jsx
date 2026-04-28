c24d3adcc9e51a458a15a4502733c5c518c53138
jagadeesh944078 <vemulajagadeesh28@gmail.com>
2026-04-28 16:18:52 +0530

    tailwind css added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,14 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="text-center mt-auto">
       <h1>Docker App 🚀</h1>
-      <button onClick={callApi}>Call Backend using React in Dev Env</button>
+      <button
+        type="button"
+        class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5"
+      >
+        Call Api
+      </button>
     </div>
   );
 };
