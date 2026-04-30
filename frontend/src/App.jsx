0b2458929b74c15ba107836fdaf7a0168433b0f6
jagadeesh944078 <vemulajagadeesh28@gmail.com>
2026-04-30 15:48:54 +0530

    auto deploy ci/cd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
         type="button"
         class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5"
       >
-        Call Api
+        Call Api In Dev
       </button>
     </div>
   );
